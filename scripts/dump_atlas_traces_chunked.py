366bb1c6a36b1089578f1cacb7865d673df97d5f
vgiotsas <giotsas@gmail.com>
2018-01-16 21:25:00 +0200

    Added stop time in measurement result url
    When downloading the results of an ongoing measurement if we don't
    define stop time it will download all results from the beginning of the
    measurement until the latest one. By using stop time we stop downloading
    results that have stopped before the given time. Note that the "stop"
    argument essentially means "stop less than" the defined time, not "stop
    exactly" at the defined time.

--- scripts/dump_atlas_traces_chunked.py
+++ scripts/dump_atlas_traces_chunked.py
@@ -1,5 +1,8 @@
-import sys,ujson
+import sys
+import ujson
+import gzip
 from urllib2 import urlopen
+
 
 class TraceAtlas:
     def __init__(self):
@@ -7,29 +10,32 @@ class TraceAtlas:
 
     def parse_measurement(self):
 
-        self.traces_fh = open(sys.argv[1], "a+")
+        self.traces_fh = gzip.open(sys.argv[1], "a+")
         # load the ip to asn mapping
         start_time = sys.argv[2]
         stop_time = sys.argv[3]
-        initial_query = "https://atlas.ripe.net/api/v2/measurements/traceroute/?start_time__gte=" + start_time + "&start_time__lte=" + stop_time + "&af=4&page_size=500"
+        allow_null_desc = False
+        if len(sys.argv) > 4:
+            allow_null_desc = bool(int(sys.argv[4]))  # set to 1 to allow null description, 0 not to allow it
+        initial_query = "https://atlas.ripe.net/api/v2/measurements/traceroute/?start_time__gte=" + \
+            start_time + "&start_time__lte=" + stop_time + "&af=4&page_size=500"
         print initial_query
         response = urlopen(initial_query).read()
         decoded_response = ujson.loads(response)
-
         last_ts = ""
         previous_last_ts = ""
         last_id = ""
 
         while 'results' in decoded_response and len(decoded_response["results"]) > 0:
             for msm_object in decoded_response['results']:
-                if msm_object["description"] is None:
+                if msm_object["description"] is None and allow_null_desc is False:
                     continue
                 last_ts = msm_object["start_time"]
                 result_url = msm_object["result"]
                 last_id = msm_object["id"]
                 try:
-                    print result_url
-                    response = urlopen(result_url)
+                    # print result_url + "?stop=" + stop_time
+                    response = urlopen(result_url + "?stop=" + stop_time)
                     CHUNK = 16 * 1024
                     while True:
                         chunk = response.read(CHUNK)
